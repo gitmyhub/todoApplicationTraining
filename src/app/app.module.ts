7310c00050afa1db0812529f44f6997c5f0cf4b0
ajay <ajay@ajays-MacBook-Pro-2.local>
2019-06-18 22:50:08 -0400

    Added Bootstrap styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
 import { ErrorComponent } from './error/error.component';
 import { ListtodosComponent } from './listtodos/listtodos.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +18,8 @@ import { ListtodosComponent } from './listtodos/listtodos.component';
     LoginComponent,
     ErrorComponent,
     ListtodosComponent,
+    MenuComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
